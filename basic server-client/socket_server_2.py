e2fe8fbd0bd72612b49d060f24cbf9ab79e131e4
unknown <chengc5@illinois.edu>
2018-09-22 16:38:37 +0800

    working

---
+++ basic server-client/socket_server_2.py
@@ -0,0 +1,15 @@
+import socketserver
+
+class MyTCPHandler (socketserver.StreamRequestHandler):
+
+    def handle(self):
+        self.data = self.rfile.readline().strip()
+        print(" {} wrote:".format(self.client_address[0]))
+        print(self.data)
+        self.wfile.write(self.data.upper())
+
+if __name__ == "__main__":
+    HOST, PORT = "localhost", 9999
+
+    with socketserver.TCPServer((HOST,PORT), MyTCPHandler) as server:
+        server.serve_forever()
